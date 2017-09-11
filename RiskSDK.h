dd2bc1da02548db168cb4e2c42366339bf2b33ae
Daniel Scheibe <daniel.scheibe@bornholdtlee.de>
2017-09-11 11:01:37 +0200

    Initial Commit

---
+++ RiskSDK.h
@@ -0,0 +1,13 @@
+//
+//  RiskSDK.h
+//  RiskSDK
+//
+//  Created by Daniel Scheibe on 11.09.17.
+//  Copyright © 2017 Risk.Ident GmbH. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RiskSDK : NSObject
+
+@end
---
+++ RiskSDK.m
@@ -0,0 +1,13 @@
+//
+//  RiskSDK.m
+//  RiskSDK
+//
+//  Created by Daniel Scheibe on 11.09.17.
+//  Copyright © 2017 Risk.Ident GmbH. All rights reserved.
+//
+
+#import "RiskSDK.h"
+
+@implementation RiskSDK
+
+@end

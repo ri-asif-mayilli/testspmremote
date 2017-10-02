095b1e3a675496637b1005d2b7d61ebe0efc71c6
Daniel Scheibe <daniel.scheibe@bornholdtlee.de>
2017-10-02 16:48:22 +0200

    changed url

--- Component/RSdkVars.swift
+++ Component/RSdkVars.swift
@@ -18,7 +18,8 @@ internal struct RSdkVars {
     internal static var EMPTY_VENDOR = "00000000-0000-0000-0000-000000000000"
     internal static var CYDIA_URL = "cydia://package/com.example.package"
     
-    internal static var DOMAIN = "https://www-test.jsctool.com"
+    internal static var SNIPPET_DOMAIN = ""
+    internal static var DOMAIN = "https://www-backup-test.jsctool.com/"
     
     internal static var POST_ENDPOINT   = "\(DOMAIN)/ios"
     internal static var ERROR_ENDPOINT  = "\(DOMAIN)/ios-err"

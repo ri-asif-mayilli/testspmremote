bb315a8a0feae335810e89b8c462be67d9b3bf0e
Daniel Scheibe <daniel.scheibe@bornholdtlee.de>
2017-10-10 11:49:43 +0200

    refactoring for obfuscation

--- Component/Sysctl.swift
+++ Component/Sysctl.swift
@@ -21,34 +21,34 @@ enum RSdkSyctlInfoType {
             return ""
             
         case .hostname:
-            return Sysctl.hostName
+            return Sysctl.sysctlHostName
             
         case .machine:
-            return Sysctl.machine
+            return Sysctl.sysctlMachine
             
         case .model:
-            return Sysctl.model
+            return Sysctl.sysctlModel
             
         case .activeCPUs:
-            return String(describing: Sysctl.activeCPUs)
+            return String(describing: Sysctl.sysctlActiveCPUs)
             
         case .osRelease:
-            return Sysctl.osRelease
+            return Sysctl.sysctlOsRelease
             
         case .osRev:
-            return String(describing: Sysctl.kernID)
+            return String(describing: Sysctl.sysctlKernID)
             
         case .osType:
-            return Sysctl.osType
+            return Sysctl.sysctlOsType
             
         case .osVersion:
-            return Sysctl.osVersion
+            return Sysctl.sysctlOsVersion
             
         case .memSize:
-            return Sysctl.memSize.description
+            return Sysctl.sysctlMemSize.description
             
         case .version:
-            return Sysctl.version
+            return Sysctl.sysctlVersion
         }
     }
 }
@@ -65,7 +65,7 @@ private struct Sysctl {
     }
     
     /// Access the raw data for an array of sysctl identifiers.
-    private static func dataForKeys(_ keys: [Int32]) throws -> [Int8] {
+    private static func dataForKeys (_ keys: [Int32]) throws -> [Int8] {
         return try keys.withUnsafeBufferPointer() { keysPointer throws -> [Int8] in
             // Preflight the request to get the required data size
             var requiredSize = 0
@@ -88,7 +88,7 @@ private struct Sysctl {
     }
     
     /// Convert a sysctl name string like "hw.memsize" to the array of `sysctl` identifiers (e.g. [CTL_HW, HW_MEMSIZE])
-    private static func keysForName(_ name: String) throws -> [Int32] {
+    private static func keysForName (_ name: String) throws -> [Int32] {
         var keysBufferSize = Int(CTL_MAXNAME)
         var keysBuffer = Array<Int32>(repeating: 0, count: keysBufferSize)
         try keysBuffer.withUnsafeMutableBufferPointer { (lbp: inout UnsafeMutableBufferPointer<Int32>) throws in
@@ -105,7 +105,7 @@ private struct Sysctl {
     }
     
     /// Invoke `sysctl` with an array of identifers, interpreting the returned buffer as the specified type. This function will throw `Error.invalidSize` if the size of buffer returned from `sysctl` fails to match the size of `T`.
-    private static func valueOfType<T>(_ type: T.Type, forKeys keys: [Int32]) throws -> T {
+    private static func valueOfType <T>(_ type: T.Type, forKeys keys: [Int32]) throws -> T {
         let buffer = try dataForKeys(keys)
         if buffer.count != MemoryLayout<T>.size {
             throw Error.invalidSize
@@ -117,7 +117,7 @@ private struct Sysctl {
     }
     
     /// Invoke `sysctl` with an array of identifers, interpreting the returned buffer as the specified type. This function will throw `Error.invalidSize` if the size of buffer returned from `sysctl` fails to match the size of `T`.
-    private static func valueOfType<T>(_ type: T.Type, forKeys keys: Int32...) throws -> T {
+    private static func valueOfType <T>(_ type: T.Type, forKeys keys: Int32...) throws -> T {
         return try valueOfType(type, forKeys: keys)
     }
     
@@ -127,7 +127,7 @@ private struct Sysctl {
     }
     
     /// Invoke `sysctl` with an array of identifers, interpreting the returned buffer as a `String`. This function will throw `Error.malformedUTF8` if the buffer returned from `sysctl` cannot be interpreted as a UTF8 buffer.
-    private static func stringForKeys(_ keys: [Int32]) throws -> String {
+    private static func stringForKeys (_ keys: [Int32]) throws -> String {
         let optionalString = try dataForKeys(keys).withUnsafeBufferPointer() { dataPointer -> String? in
             dataPointer.baseAddress.flatMap { String(validatingUTF8: $0) }
         }
@@ -138,22 +138,22 @@ private struct Sysctl {
     }
     
     /// Invoke `sysctl` with an array of identifers, interpreting the returned buffer as a `String`. This function will throw `Error.malformedUTF8` if the buffer returned from `sysctl` cannot be interpreted as a UTF8 buffer.
-    private static func stringForKeys(_ keys: Int32...) throws -> String {
+    private static func stringForKeys (_ keys: Int32...) throws -> String {
         return try stringForKeys(keys)
     }
     
     /// Invoke `sysctl` with the specified name, interpreting the returned buffer as a `String`. This function will throw `Error.malformedUTF8` if the buffer returned from `sysctl` cannot be interpreted as a UTF8 buffer.
-    private static func stringForName(_ name: String) throws -> String {
+    private static func stringForName (_ name: String) throws -> String {
         return try stringForKeys(keysForName(name))
     }
     
     /// e.g. "MyComputer.local" (from System Preferences -> Sharing -> Computer Name) or
     /// "My-Name-iPhone" (from Settings -> General -> About -> Name)
-    internal static var hostName: String { return try! Sysctl.stringForKeys([CTL_KERN, KERN_HOSTNAME]) }
+    internal static var sysctlHostName : String { return try! Sysctl.stringForKeys([CTL_KERN, KERN_HOSTNAME]) }
     
     /// e.g. "x86_64" or "N71mAP"
     /// NOTE: this is *corrected* on iOS devices to fetch hw.model
-    internal static var machine: String {
+    internal static var sysctlMachine : String {
         #if os(iOS) && !arch(x86_64) && !arch(i386)
             return try! Sysctl.stringForKeys([CTL_HW, HW_MODEL])
         #else
@@ -163,7 +163,7 @@ private struct Sysctl {
     
     /// e.g. "MacPro4,1" or "iPhone8,1"
     /// NOTE: this is *corrected* on iOS devices to fetch hw.machine
-    internal static var model: String {
+    internal static var sysctlModel : String {
         #if os(iOS) && !arch(x86_64) && !arch(i386)
             return try! Sysctl.stringForKeys([CTL_HW, HW_MACHINE])
         #else
@@ -171,30 +171,30 @@ private struct Sysctl {
         #endif
     }
     
-    internal static var machineArch: String { return try! Sysctl.stringForKeys([CTL_HW, HW_MACHINE_ARCH]) }
+    internal static var sysctlMachineArch : String { return try! Sysctl.stringForKeys([CTL_HW, HW_MACHINE_ARCH]) }
     
-    internal static var activeCPUs: Int32 { return try! Sysctl.valueOfType(Int32.self, forKeys: [CTL_HW, HW_AVAILCPU]) }
+    internal static var sysctlActiveCPUs : Int32 { return try! Sysctl.valueOfType(Int32.self, forKeys: [CTL_HW, HW_AVAILCPU]) }
     
     /// e.g. "15.3.0" or "15.0.0"
-    internal static var osRelease: String { return try! Sysctl.stringForKeys([CTL_KERN, KERN_OSRELEASE]) }
+    internal static var sysctlOsRelease : String { return try! Sysctl.stringForKeys([CTL_KERN, KERN_OSRELEASE]) }
     
     /// e.g. 199506 or 199506
-    internal static var osRev: Int32 { return try! Sysctl.valueOfType(Int32.self, forKeys: [CTL_KERN, KERN_OSREV]) }
+    internal static var sysctlOsRev : Int32 { return try! Sysctl.valueOfType(Int32.self, forKeys: [CTL_KERN, KERN_OSREV]) }
     
     /// e.g. "Darwin" or "Darwin"
-    internal static var osType: String { return try! Sysctl.stringForKeys([CTL_KERN, KERN_OSTYPE]) }
+    internal static var sysctlOsType : String { return try! Sysctl.stringForKeys([CTL_KERN, KERN_OSTYPE]) }
     
-    internal static var kernID: Int32 { return try! Sysctl.valueOfType(Int32.self, forKeys: [CTL_KERN, KERN_SAVED_IDS]) }
+    internal static var sysctlKernID : Int32 { return try! Sysctl.valueOfType(Int32.self, forKeys: [CTL_KERN, KERN_SAVED_IDS]) }
     
     /// e.g. "15D21" or "13D20"
-    internal static var osVersion: String { return try! Sysctl.stringForKeys([CTL_KERN, KERN_OSVERSION]) }
+    internal static var sysctlOsVersion : String { return try! Sysctl.stringForKeys([CTL_KERN, KERN_OSVERSION]) }
     
     /// e.g. "Darwin Kernel Version 15.3.0: Thu Dec 10 18:40:58 PST 2015; root:xnu-3248.30.4~1/RELEASE_X86_64" or
     /// "Darwin Kernel Version 15.0.0: Wed Dec  9 22:19:38 PST 2015; root:xnu-3248.31.3~2/RELEASE_ARM64_S8000"
-    internal static var version: String { return try! Sysctl.stringForKeys([CTL_KERN, KERN_VERSION]) }
+    internal static var sysctlVersion : String { return try! Sysctl.stringForKeys([CTL_KERN, KERN_VERSION]) }
     
-    internal static var domainName: String { return try! Sysctl.stringForKeys([CTL_KERN, KERN_DOMAINNAME]) }
+    internal static var sysctlDomainName : String { return try! Sysctl.stringForKeys([CTL_KERN, KERN_DOMAINNAME]) }
     
-    internal static var memSize: UInt64 { return try! Sysctl.valueOfType(UInt64.self, forKeys: [CTL_HW, HW_MEMSIZE]) }
+    internal static var sysctlMemSize : UInt64 { return try! Sysctl.valueOfType(UInt64.self, forKeys: [CTL_HW, HW_MEMSIZE]) }
     
 }

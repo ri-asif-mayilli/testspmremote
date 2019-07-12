a8cae055bfb6bb40d5f9bc93fbbcf2824d7772c0
alexandervanderwerff <alexander.van.der.werff@riskident.com>
2019-07-12 09:48:51 +0200

    set sdk version to 1.3.5

--- Component/RSdkVars.swift
+++ Component/RSdkVars.swift
@@ -10,7 +10,7 @@ internal struct RSdkVars {
     
     internal static var SDKVERSION : String {
         
-        return "1.3.4"
+        return "1.3.5"
     }
     
     internal struct RequestManagerVars {

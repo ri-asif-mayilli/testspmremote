ec3e3dc96830716c11fc1bd5c823445dcee25e83
alexandervanderwerff <alexander.van.der.werff@riskident.com>
2019-10-17 10:01:36 +0200

    ClientSecurityKit: set version to 1.4.3

--- Component/RSdkVars.swift
+++ Component/RSdkVars.swift
@@ -10,7 +10,7 @@ internal struct RSdkVars {
     
     internal static var SDKVERSION : String {
         
-        return "1.4.2"
+        return "1.4.3"
     }
     
     internal struct RequestManagerVars {

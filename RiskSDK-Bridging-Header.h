98741c9adcf7309e9f1b563705d06e2eeaaf5343
Asif Mayilli <asifmayilli@Asifs-MacBook-Pro.local>
2020-06-24 14:49:27 +0200

    removed submodule config from ios-sdk

--- RiskSDK-Bridging-Header.h
+++
@@ -1,8 +0,0 @@
-/*
- * This file is subject to the terms and conditions defined in
- * the LICENSE file that is distributed in the same package.
- */
-//
-//  Use this file to import your target's public headers that you would like to expose to Swift.
-//"
-
